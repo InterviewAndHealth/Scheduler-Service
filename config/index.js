c4021280e9aef744212e7468c8210dce8f43a4fc
GopalSaraf <sarafgopal37@gmail.com>
2024-11-25 19:30:02 +0530

    Added workflow and updated env names

--- config/index.js
+++ config/index.js
@@ -3,9 +3,23 @@ const dotEnv = require("dotenv");
 dotEnv.config();
 
 module.exports = {
-  REDIS_URL: process.env.REDIS_URL,
-  RABBITMQ_URL: process.env.RABBITMQ_URL,
+  REDIS_USERNAME: process.env.REDIS_USERNAME,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_URL:
+    process.env.REDIS_URL ||
+    `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+
+  RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
+  RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
+  RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+  RABBITMQ_PORT: process.env.RABBITMQ_PORT,
+  RABBITMQ_URL:
+    process.env.RABBITMQ_URL ||
+    `amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+
   EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  SERVICE_QUEUE: process.env.SERVICE_QUEUE,
+  SERVICE_NAME: process.env.SERVICE_NAME || "SCHEDULER_SERVICE",
+  SERVICE_QUEUE: process.env.SERVICE_QUEUE || process.env.SCHEDULER_QUEUE,
 };
