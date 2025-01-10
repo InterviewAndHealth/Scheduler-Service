668bafc708116f6844949a392b030ebf5fd11e99
GopalSaraf <sarafgopal37@gmail.com>
2025-01-10 23:21:56 +0530

    Updated broker connection

--- services/broker/broker.js
+++ services/broker/broker.js
@@ -3,39 +3,55 @@ const { RABBITMQ_URL, EXCHANGE_NAME } = require("../../config");
 const { Logger } = require("../../utils");
 
 class Broker {
-  static #channel;
-  static #exchange;
+  static #connection;
 
   /**
    * Connect to RabbitMQ
-   * @returns {Promise} - A promise that resolves to a RabbitMQ channel
+   * @returns {Promise} - A promise that resolves to a RabbitMQ connection
    * @throws {Error} - If RabbitMQ connection fails
    */
   static async connect() {
     try {
-      if (this.#channel) return this.#channel;
+      if (this.#connection) return this.#connection;
       const connection = await amqplib.connect(RABBITMQ_URL);
-      const channel = await connection.createChannel();
-      this.#channel = channel;
+
+      connection.on("error", (err) => {
+        Logger.error("RabbitMQ connection error", err);
+      });
+
+      connection.on("close", () => {
+        Logger.error("RabbitMQ connection closed");
+        this.#connection = null;
+
+        setTimeout(async () => {
+          Logger.info("Reconnecting to RabbitMQ");
+          await this.connect();
+        }, 1000);
+      });
+
+      connection.on("blocked", (reason) => {
+        Logger.error("RabbitMQ connection blocked", reason);
+      });
+
+      this.#connection = connection;
       Logger.info("Connected to RabbitMQ");
-      return channel;
+      return connection;
     } catch (err) {
       Logger.error("Failed to connect to RabbitMQ", err);
     }
   }
 
   /**
-   * Create a RabbitMQ exchange and channel
-   * @returns {Promise} - A promise that resolves to a RabbitMQ exchange
-   * @throws {Error} - If RabbitMQ queue creation fails
+   * Create a RabbitMQ channel
+   * @returns {Promise} - A promise that resolves to a RabbitMQ channel
+   * @throws {Error} - If RabbitMQ channel creation fails
    */
   static async channel() {
     try {
-      if (this.#exchange) return this.#exchange;
-      const channel = await this.connect();
+      const connection = await this.connect();
+      const channel = await connection.createChannel();
       await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: true });
-      this.#exchange = channel;
-      Logger.info("Created RabbitMQ exchange");
+      Logger.info("Created RabbitMQ channel");
       return channel;
     } catch (err) {
       Logger.error("Failed to create RabbitMQ channel", err);
